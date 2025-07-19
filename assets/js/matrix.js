e4c995d4ee08bc06c9900bbf49c03c9e3942cb8b
zerocouponhero <venk100304@gmail.com>
2025-07-20 01:50:18 +1000

    Create matrix.js

---
+++ assets/js/matrix.js
@@ -0,0 +1,29 @@
+const canvas = document.getElementById("matrix");
+const ctx = canvas.getContext("2d");
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const letters = "アァイィウエカキサシタチナニハマヤラワガザダバパ0123456789";
+const fontSize = 14;
+const columns = canvas.width / fontSize;
+const drops = new Array(Math.floor(columns)).fill(1);
+
+function draw() {
+  ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#0f0";
+  ctx.font = fontSize + "px monospace";
+
+  drops.forEach((y, i) => {
+    const text = letters[Math.floor(Math.random() * letters.length)];
+    const x = i * fontSize;
+    ctx.fillText(text, x, y * fontSize);
+    if (y * fontSize > canvas.height && Math.random() > 0.975) {
+      drops[i] = 0;
+    }
+    drops[i]++;
+  });
+}
+
+setInterval(draw, 40);
